223be42e8fa22ae2e433fec709be6c0cb8daccc2
hayversong <hayversoon@gmail.com>
2026-05-21 17:02:43 -0300

    feat: barra de progresso para projeto

--- internal/model/project.go
+++ internal/model/project.go
@@ -5,3 +5,23 @@ type Project struct {
 	Name  string
 	Cards []Card
 }
+
+func (p Project) Progress() int {
+
+	total := len(p.Cards)
+
+	if total == 0 {
+		return 0
+	}
+
+	done := 0
+
+	for _, card := range p.Cards {
+
+		if card.Status == "done" {
+			done++
+		}
+	}
+
+	return (done * 100) / total
+}
